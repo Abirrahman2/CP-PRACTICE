f8e829bb3ce456a1b42f2769d1455f3f4f517c4d
MD MAHMUDUR RAHMAN <105537287+Abirrahman2@users.noreply.github.com>
2025-09-22 22:26:39 +0600

    Add files via upload

---
+++ Dynamic Programming/CSES_Longest_Common_Subsequence.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    //cin>>t;
+    t=1;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        int brr[m];
+        for(int i=0;i<m;i++)
+        {
+            cin>>brr[i];
+        }
+       vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+       for(int i=0;i<=n;i++)
+       {
+           dp[i][0]=0;
+       }
+       for(int i=0;i<=m;i++)
+       {
+           dp[0][i]=0;
+       }
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=m;j++)
+           {
+               if(arr[i-1]==brr[j-1])
+               {
+                   dp[i][j]=1+dp[i-1][j-1];
+               }
+               else
+               {
+
+                   dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+               }
+
+           }
+       }
+       cout<<dp[n][m]<<endl;
+       int i=n;
+       int j=m;
+       vector<int>ans;
+       while(i>0 && j>0)
+       {
+           if(arr[i-1]==brr[j-1])
+           {
+            ans.push_back(arr[i-1]);
+            i--;
+            j--;
+           }
+           else{
+              if(dp[i-1][j]>dp[i][j-1])
+              {
+                  i--;
+              }
+              else{
+                j--;
+              }
+           }
+       }
+       reverse(ans.begin(),ans.end());
+       for(auto it:ans)
+       {
+           cout<<it<<" ";
+       }
+       cout<<endl;
+    }
+}
+
